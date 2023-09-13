fc9ce6c306e709a9235c7344068383a6922b3a58
Marcos Cordeiro Pires <marcosccpiress15@gmail.com>
2023-09-13 11:19:55 -0300

    Update routes.js

--- API_CRUD_BANCO/src/routes/routes.js
+++ API_CRUD_BANCO/src/routes/routes.js
@@ -20,18 +20,5 @@ router.get('/contratos',TaskController.listarContratos)
 router.get('/contratos/:id',TaskController.listarUmContrato)
 router.delete('/contratos/:id',TaskController.excluirContrato)
 router.put('/contratos/:id',TaskController.atualizarContrato)
-router.post('/upload', upload.single('file'), (req,res) => {
-    try {
-        const file = req.file
-        if (!file) {
-          return res.status(400).json({ message: 'Nenhum arquivo foi enviado.' })
-        }
-        // Você pode fazer o que quiser com o arquivo aqui, como salvar no banco de dados, processá-lo, etc.
-        return res.status(200).json({ message: 'Arquivo enviado com sucesso.' })
-    } catch (error) {
-        console.error('Erro ao fazer upload do arquivo:', error)
-        return res.status(500).json({ message: 'Erro interno do servidor.' })
-    }
-})
 
 module.exports = router
